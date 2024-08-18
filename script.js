3be09c9fe0c74619a6dfd221c5a6a565d3d49cc7
gustavolucenadev <gus_duarte8@hotmail.com>
2024-08-18 20:27:51 -0300

    fix: changing styles

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.getElementById('menu-toggle');
     const nav = document.getElementById('nav');
@@ -7,3 +8,13 @@ document.addEventListener('DOMContentLoaded', () => {
         nav.classList.toggle('open');
     });
 });
+
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) { // Ajuste o valor conforme necessário
+        header.classList.add('shrink');
+    } else {
+        header.classList.remove('shrink');
+    }
+});
+
